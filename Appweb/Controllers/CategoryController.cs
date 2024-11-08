970cd0320e8535b2bcd575d1af52eb152e0f6f37
Tan Phat <impphatdn99@gmail.com>
2024-11-08 10:37:42 +0700

    Load category table on web application

--- Appweb/Controllers/CategoryController.cs
+++ Appweb/Controllers/CategoryController.cs
@@ -1,10 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Appweb.Data;
+using Appweb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Appweb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDBContext _dbContext;
+        public CategoryController(ApplicationDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public IActionResult Index()
+        {
+            List<Category> listCategory = _dbContext.Categories.ToList();
+            return View(listCategory);
+        }
+        public IActionResult Edit(int id)
         {
             return View();
         }
